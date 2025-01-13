c679473a49f6b9d093fef03ba68d226cbe28202f
Vlad <92322447+vaikcode@users.noreply.github.com>
2025-01-13 17:57:45 +0300

    Create sml-crom.sh

---
+++ sml-crom.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# sml-crom.sh
+#
+# http://212.3.149.253/sml/sml-crom.sh
+#
+
+mount -o remount,rw /
+
+SMLFOLDER=/sml
+
+if [ -f /usr/local/sbin/pistar-daily.cron ];then
+	sed -i "s/^\/usr\/local\/sbin\/HostFilesUpdate.sh.*/\/usr\/local\/sbin\/HostFilesUpdate.sh;sml-gethost;sml-getdmrid;/" /usr/local/sbin/pistar-daily.cron
+fi
+
+if [ ! -L /etc/systemd/system/sml.service ]; then
+	ln -s ${SMLFOLDER}/unit/sml.service /etc/systemd/system/sml.service
+fi
+
+if [ ! -L /etc/systemd/system/sml.timer ]; then
+	ln -s ${SMLFOLDER}/unit/sml.timer /etc/systemd/system/sml.timer
+fi
+
+systemctl daemon-reload
+systemctl enable sml.timer &>/dev/null
+systemctl start sml.timer
+
+exit 0
