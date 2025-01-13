2c4e540d6b0d76a78e61ceb884212fff24890ef4
Vlad <92322447+vaikcode@users.noreply.github.com>
2025-01-13 20:51:48 +0300

    Update setup_sml_scripts.sh

--- setup_sml_scripts.sh
+++ setup_sml_scripts.sh
@@ -4,23 +4,23 @@
 #
 #
 
-rm -r /var/www/html/sml
+rm -r /var/www/html/xlxd/sml
 
-mkdir /var/www/html/sml
-mkdir /var/www/html/sml/unit
+mkdir /var/www/html/xlxd/sml
+mkdir /var/www/html/xlxd/sml/unit
 
 cd sever83_sml_scripts
 
-cp index.html /var/www/html/sml/index.html
-cp index2.html /var/www/html/sml/unit/index.html
+cp index.html /var/www/html/xlxd/sml/index.html
+cp index2.html /var/www/html/xlxd/sml/unit/index.html
 
-cp sml.sh /var/www/html/sml/sml.sh
-cp sml-del.sh /var/www/html/sml/sml-del.sh
-cp sml-update.sh /var/www/html/sml/sml-update.sh
-cp sml-cron.sh /var/www/html/sml/sml-cron.sh
-cp sml-getdmrid.sh /var/www/html/sml/sml-getdmrid.sh
-cp sml-gethost.sh /var/www/html/sml/sml-gethost.sh
+cp sml.sh /var/www/html/xlxd/sml/sml.sh
+cp sml-del.sh /var/www/html/xlxd/sml/sml-del.sh
+cp sml-update.sh /var/www/html/xlxd/sml/sml-update.sh
+cp sml-cron.sh /var/www/html/xlxd/sml/sml-cron.sh
+cp sml-getdmrid.sh /var/www/html/xlxd/sml/sml-getdmrid.sh
+cp sml-gethost.sh /var/www/html/xlxd/sml/sml-gethost.sh
 
-cp sml.service /var/www/html/sml/unit/sml.service
-cp sml.timer /var/www/html/sml/unit/sml.timer
+cp sml.service /var/www/html/xlxd/sml/unit/sml.service
+cp sml.timer /var/www/html/xlxd/sml/unit/sml.timer
 
