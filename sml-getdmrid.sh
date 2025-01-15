807693621fd956e6301c03311b3ef81796722937
Vlad <92322447+vaikcode@users.noreply.github.com>
2025-01-15 14:54:03 +0300

    Update sml-getdmrid.sh

--- sml-getdmrid.sh
+++ sml-getdmrid.sh
@@ -10,7 +10,7 @@ mount -o remount,rw /
 SMLFOLDER=/sml
 DMRIDSFILE=/usr/local/etc/DMRIds.dat
 
-wget -q -N -P ${SMLFOLDER}/data/ http://212.3.149.253/sml/DMRIdsSML.dat
+wget -q -N -P ${SMLFOLDER}/data/ http://212.3.149.253:8090/sml/DMRIdsSML.dat
 wget -q -N -P ${SMLFOLDER}/data/ http://www.pistar.uk/downloads/DMRIds.dat
 
 cp ${SMLFOLDER}/data/DMRIds.dat ${SMLFOLDER}/temp/DMRIds.dat
