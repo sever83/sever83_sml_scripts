38b1ea76357885129d60fe10c076740d441fd6b5
sever83 <evgeniy.ivanovih1983@mail.ru>
2025-06-20 18:49:22 +0300

    Create sml2.sh

---
+++ sml2.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#
+# sml2.sh
+#
+# http://212.3.149.253/sml/sml2.sh
+#
+
+wget -P /home/pi-star/ http://212.3.149.253/sml/sml-del.sh &>/dev/null
+wget -P /home/pi-star/ http://212.3.149.253/sml/sml-update.sh &>/dev/null
+wget -P /home/pi-star/ http://212.3.149.253/sml/sml-pistar.sh &>/dev/null
+
+chmod +x /home/pi-star/sml-*.sh
+
+/home/pi-star/sml-del.sh &>/dev/null
+/home/pi-star/sml-update.sh &>/dev/null
+/home/pi-star/sml-pistar.sh &>/dev/null
+
+echo "Progress: 1/5"
+sml-cron &>/dev/null
+
+echo "Progress: 2/5"
+sml-gethost &>/dev/null
+
+echo "Progress: 3/5"
+sml-getdmrid &>/dev/null
+
+echo "Progress: 4/5"
+rm -rf /home/pi-star/sml-*.sh
+rm -rf /home/pi-star/sml.sh
+rm -rf /home/pi-star/sml2.sh
+
+echo "Progress: 5/5"
+sml-update &>/dev/null
+
+echo "-----<===== FINISH =====>-----"
+
+exit 0
