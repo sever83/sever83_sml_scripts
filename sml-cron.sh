8fb93791e80ee7c6ef5884f353c8e92cab8a7f0b
sever83 <evgeniy.ivanovih1983@mail.ru>
2025-06-20 18:44:57 +0300

    Update sml-cron.sh

--- sml-cron.sh
+++ sml-cron.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env bash
-#
-# sml-crom.sh
-#
-# http://212.3.149.253/sml/sml-crom.sh
-#
 
 mount -o remount,rw /
 
-SMLFOLDER=/sml
+SMLFOLDER=/tmp/sml
 
-if [ -f /usr/local/sbin/pistar-daily.cron ]; then
+if [ -f /usr/local/sbin/pistar-daily.cron ];then
 	sed -i "s/^\/usr\/local\/sbin\/HostFilesUpdate.sh.*/\/usr\/local\/sbin\/HostFilesUpdate.sh;sml-gethost;sml-getdmrid;/" /usr/local/sbin/pistar-daily.cron
 fi
 
 if [ ! -L /etc/systemd/system/sml.service ]; then
-	ln -s ${SMLFOLDER}/unit/sml.service /etc/systemd/system/sml.service
+	ln -s ${smlFOLDER}/unit/sml.service /etc/systemd/system/sml.service
 fi
 
 if [ ! -L /etc/systemd/system/sml.timer ]; then
-	ln -s ${SMLFOLDER}/unit/sml.timer /etc/systemd/system/sml.timer
+	ln -s ${smlFOLDER}/unit/sml.timer /etc/systemd/system/sml.timer
 fi
 
 systemctl daemon-reload
