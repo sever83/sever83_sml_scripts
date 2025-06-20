5fce662e8d97ef5026d2dd345a63db4f141ac981
sever83 <evgeniy.ivanovih1983@mail.ru>
2025-06-20 18:46:00 +0300

    Update sml-getdmrid.sh

--- sml-getdmrid.sh
+++ sml-getdmrid.sh
@@ -1,26 +1,13 @@
 #!/usr/bin/env bash
-#
-# sml-getdmrid.sh
-#
-# http://212.3.149.253/sml/sml-getdmrid.sh
-#
 
 mount -o remount,rw /
 
-SMLFOLDER=/sml
+SMLFOLDER=/tmp/sml
 DMRIDSFILE=/usr/local/etc/DMRIds.dat
 
 wget -q -N -P ${SMLFOLDER}/data/ http://212.3.149.253/sml/DMRIdsSML.dat
-wget -q -N -P ${SMLFOLDER}/data/ http://www.pistar.uk/downloads/DMRIds.dat
 
-#cp ${SMLFOLDER}/data/DMRIds.dat ${SMLFOLDER}/temp/DMRIds.dat
-#sed -i "/^103.*/d" ${SMLFOLDER}/temp/DMRIds.dat
-#cat ${SMLFOLDER}/data/DMRIdsSML.dat ${SMLFOLDER}/temp/DMRIds.dat > ${DMRIDSFILE}
-
-#sed -e 's;/;'$'\t';g'
-#sed -i "/^103.*/d" ${SMLFOLDER}/temp/DMRIds.dat
-
-#sed -i "s;/;'$'\t';g" ${SMLFOLDER}/data/DMRIdsSML.dat
-cp ${SMLFOLDER}/data/DMRIdsSML.dat ${DMRIDSFILE}
+cp ${SMLFOLDER}/data/DMRIds.dat ${SMLFOLDER}/temp/DMRIds.dat
+cat ${SMLFOLDER}/data/DMRIdsSML.dat ${SMLFOLDER}/temp/DMRIds.dat > ${DMRIDSFILE}
 
 exit 0
