3cfb4d8a4e274be10ebfb29cfe74b6c4db89cb49
sever83 <evgeniy.ivanovih1983@mail.ru>
2025-06-27 01:38:52 +0300

    Update sml-getdmrid.sh

--- sml-getdmrid.sh
+++ sml-getdmrid.sh
@@ -6,7 +6,7 @@ SMLFOLDER=/tmp/sml
 DMRIDSFILE=/usr/local/etc/DMRIds.dat
 
 wget -q -N -P ${SMLFOLDER}/data/ http://212.3.149.253/sml/DMRIdsSML.dat
-
+wget -q -N -P ${SMLFOLDER}/data/ http://212.3.149.253/downloads/DMRIds.dat
 cp ${SMLFOLDER}/data/DMRIds.dat ${SMLFOLDER}/temp/DMRIds.dat
 cat ${SMLFOLDER}/data/DMRIdsSML.dat ${SMLFOLDER}/temp/DMRIds.dat > ${DMRIDSFILE}
 
