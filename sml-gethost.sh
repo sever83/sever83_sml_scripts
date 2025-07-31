03bc2f855d9596111137ac0f2b07ce5d70c45559
sever83 <evgeniy.ivanovih1983@mail.ru>
2025-07-31 11:10:40 +0300

    Update sml-gethost.sh

--- sml-gethost.sh
+++ sml-gethost.sh
@@ -8,16 +8,16 @@ mount -o remount,rw /
 sed -i "/^391.*/d" /usr/local/etc/XLXHosts.txt
 echo "391;212.3.149.253;4004" >> /usr/local/etc/XLXHosts.txt
 sed -i "/^39100.*/d" /usr/local/etc/YSFHosts.txt
-echo "39100;RU-XLX391;XLX SML-MSK;212.3.149.253;42000;000;" >> /usr/local/etc/YSFHosts.txt
-echo "39100;RU-XLX391;XLX SML-MSK;212.3.149.253;42000;000;" > /root/YSFHosts.txt
+echo "39100;RU-XLX391;XLX SMK-MSK;212.3.149.253;42000;000;" >> /usr/local/etc/YSFHosts.txt
+echo "39100;RU-XLX391;XLX SMK-MSK;212.3.149.253;42000;000;" > /root/YSFHosts.txt
 sed -i "/^XLX_391.*/d" /usr/local/etc/DMR_Hosts.txt
 echo "XLX_391 0000 212.3.149.253 passw0rd 62030" >> /usr/local/etc/DMR_Hosts.txt
 sed -i "/^SML.*/d" /usr/local/etc/XLXHosts.txt
 echo "SML;212.3.149.253;4004" >> /usr/local/etc/XLXHosts.txt
 echo "SML;212.3.149.253;4004" > /root/XLXHosts.txt
 sed -i "/^XLX_SML.*/d" /usr/local/etc/DMR_Hosts.txt
-echo "XLX_SML 0000 212.3.149.253 passw0rd 62030" >> /usr/local/etc/DMR_Hosts.txt
-echo "XLX_SML 0000 212.3.149.253 passw0rd 62030" > /root/DMR_Hosts.txt
+echo "XLX_SMK 0000 212.3.149.253 passw0rd 62030" >> /usr/local/etc/DMR_Hosts.txt
+echo "XLX_SMK 0000 212.3.149.253 passw0rd 62030" > /root/DMR_Hosts.txt
 sed -i "/^391*/d" /usr/local/etc/P25Hosts.txt
 echo "391 212.3.149.253 41000" >> /usr/local/etc/P25Hosts.txt
 echo "391 212.3.149.253 41000" > /root/P25Hosts.txt
