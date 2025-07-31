aa049cdd2d81ea8b6a60241cb40aa9ea2ce57a38
sever83 <evgeniy.ivanovih1983@mail.ru>
2025-07-31 09:21:47 +0300

    Update sml-cron.sh

--- sml-cron.sh
+++ sml-cron.sh
@@ -1,23 +1,21 @@
 #!/usr/bin/env bash
-
+#
+# sml-crom.sh
+#
+# http://212.3.149.253/sml/sml-crom.sh
+#
 mount -o remount,rw /
-
-SMLFOLDER=/tmp/sml
-
+SMLFOLDER=/sml
 if [ -f /usr/local/sbin/pistar-daily.cron ]; then
-	sed -i "s/^\/usr\/local\/sbin\/HostFilesUpdate.sh.*/\/usr\/local\/sbin\/HostFilesUpdate.sh;sml-gethost;sml-getdmrid;/" /usr/local/sbin/pistar-daily.cron
+    sed -i "s/^\/usr\/local\/sbin\/HostFilesUpdate.sh.*/\/usr\/local\/sbin\/HostFilesUpdate.sh;sml-gethost;sml-getdmrid;/" /usr/local/sbin/pistar-daily.cron
 fi
-
 if [ ! -L /etc/systemd/system/sml.service ]; then
-	ln -s ${smlFOLDER}/unit/sml.service /etc/systemd/system/sml.service
+    ln -s ${SMLFOLDER}/unit/sml.service /etc/systemd/system/sml.service
 fi
-
 if [ ! -L /etc/systemd/system/sml.timer ]; then
-	ln -s ${smlFOLDER}/unit/sml.timer /etc/systemd/system/sml.timer
+    ln -s ${SMLFOLDER}/unit/sml.timer /etc/systemd/system/sml.timer
 fi
-
 systemctl daemon-reload
 systemctl enable sml.timer &>/dev/null
 systemctl start sml.timer
-
 exit 0
