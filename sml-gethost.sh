779a16d55f2f95bbba94d3d046efc6ee73299e71
sever83 <evgeniy.ivanovih1983@mail.ru>
2025-07-31 11:19:30 +0300

    Update sml-gethost.sh

--- sml-gethost.sh
+++ sml-gethost.sh
@@ -15,7 +15,7 @@ echo "XLX_391 0000 212.3.149.253 passw0rd 62030" >> /usr/local/etc/DMR_Hosts.txt
 sed -i "/^SMK.*/d" /usr/local/etc/XLXHosts.txt
 echo "SMK;212.3.149.253;4004" >> /usr/local/etc/XLXHosts.txt
 echo "SMK;212.3.149.253;4004" > /root/XLXHosts.txt
-sed -i "/^XLX_SML.*/d" /usr/local/etc/DMR_Hosts.txt
+sed -i "/^XLX_SMK.*/d" /usr/local/etc/DMR_Hosts.txt
 echo "XLX_SMK 0000 212.3.149.253 passw0rd 62030" >> /usr/local/etc/DMR_Hosts.txt
 echo "XLX_SMK 0000 212.3.149.253 passw0rd 62030" > /root/DMR_Hosts.txt
 sed -i "/^391*/d" /usr/local/etc/P25Hosts.txt
